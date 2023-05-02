0775ed8caccab6e0c4b128016c5125322e9c3b44
Product impact A&C <118974862+productimpact@users.noreply.github.com>
2023-05-02 18:39:06 +0300

    added files

---
+++ Web Developer - Lesson 35 - PHP Admin page/index.php
@@ -0,0 +1,33 @@
+<?php
+
+include "functions/functions.php";
+
+?>
+
+<html>
+
+<head>
+    <title>Example of an homepage</title>
+</head>
+
+<body>
+    <!-- the header.php file -->
+    <?php
+    include "assets/php/header.php"
+    ?>
+
+    <!-- the nav.php file -->
+    <?php
+    include "assets/php/nav.php"
+    ?>
+
+    <!-- the index.php file -->
+    <div>
+        Homepage content
+    </div>
+</body>
+
+</html>
+
+
+
